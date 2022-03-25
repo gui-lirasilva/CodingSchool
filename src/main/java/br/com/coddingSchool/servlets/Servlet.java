26410767d09816be668727f3461d7e629489c179
Guilherme Henrique <guilira.dev@gmail.com>
2022-03-25 16:06:24 -0300

    Ajustando servlet e JSP

--- src/main/java/br/com/coddingSchool/servlets/Servlet.java
+++ src/main/java/br/com/coddingSchool/servlets/Servlet.java
@@ -1,5 +1,9 @@
 package br.com.coddingSchool.servlets;
 
+import br.com.coddingSchool.dao.CategoryDao;
+import br.com.coddingSchool.model.Category;
+import br.com.coddingSchool.util.JpaUtil;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,12 +11,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ListaCategorias", value = "/listaCategorias")
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/CategoryList.jsp");
+        CategoryDao categoryDao = new CategoryDao(JpaUtil.getEntityManager());
+        List<Category> categoryList = categoryDao.listAll();
+
+        request.setAttribute("categories", categoryList);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/categoryList.jsp");
         requestDispatcher.forward(request, response);
     }
 }
---
+++ src/main/webapp/categoryList.jsp
@@ -0,0 +1,36 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+    <head>
+        <title>Title</title>
+    </head>
+    <body>
+        <table border="1">
+            <tr>
+                <td>Id</td>
+                <td>Name</td>
+                <td>Code</td>
+                <td>Order</td>
+                <td>Description</td>
+                <td>Active</td>
+                <td>Icon</td>
+                <td>Color</td>
+                <td>Study guide</td>
+            </tr>
+            <c:forEach items="${categories}" var="Categoria">
+                <tr>
+                    <td>${Categoria.id}</td>
+                    <td>${Categoria.name}</td>
+                    <td>${Categoria.code}</td>
+                    <td>${Categoria.order}</td>
+                    <td>${Categoria.description}</td>
+                    <td>${Categoria.active}</td>
+                    <td>${Categoria.iconPath}</td>
+                    <td>${Categoria.colorCode}</td>
+                    <td>${Categoria.studyGuide}</td>
+                </tr>
+            </c:forEach>
+        </table>
+    </body>
+</html>
