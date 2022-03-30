370e9120b802e3fa326397f8c24eca88fc7de20c
Guilherme Henrique <guilira.dev@gmail.com>
2022-03-30 16:58:33 -0300

    Refatorando código javascript

--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -2,8 +2,8 @@ function switchStatus(event, categoryId) {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', '/alteraVisibilidade?id='+categoryId);
     xhr.addEventListener('load', function() {
-        if (xhr.status===204) {
-            const activeStatus = document.querySelector('#active'+categoryId);
+        if (xhr.status === 204) {
+            const activeStatus = document.querySelector('#active' + categoryId);
             if (activeStatus.textContent === 'true') {
                 activeStatus.textContent = 'false';
             } else {
